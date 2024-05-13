878972b9247657ec903fb640ef280c39c39cd781
syleung <syleung1220@gmail.com>
2024-05-13 20:04:27 +0800

    customing

--- cloud_chatbot.py
+++ cloud_chatbot.py
@@ -4,6 +4,16 @@ import requests
 import streamlit as st
 import textract
 
+# custom login component
+import time
+from typing import Optional
+from streamlit_authenticator.utilities.validator import Validator
+from streamlit_authenticator.utilities.exceptions import DeprecationError
+from streamlit_authenticator.authenticate.cookie import CookieHandler
+from streamlit_authenticator.authenticate.authentication import AuthenticationHandler
+from customAuthentication import CustomAuthenticationHandler
+# custom login component
+
 import yaml
 from yaml.loader import SafeLoader
 import streamlit_authenticator as stauth
@@ -19,7 +29,78 @@ apis = {
     "TRANSLATE": "https://httpbin.org/get",# "https://v1/translate",
     "DRAW":  "https://httpbin.org/get",# "https://v1/media",
     "MUSIC": "https://httpbin.org/get",#  "https://v1/music",
+    "LOGIN": "http://127.0.0.1:8080/function/login",
 }
+
+
+# ---------- def custom login widget ------------
+class CustomAuthenticate:
+    def __init__(self):
+        """
+        Create a new instance of "Authenticate".
+        """
+        self.authentication_handler     =   CustomAuthenticationHandler()
+        self.cookie_handler             =   CookieHandler("random_cookie_name", "random_signature_key", 30)
+
+
+    def customLogin(self, location: str='main', max_concurrent_users: Optional[int]=None,
+                max_login_attempts: Optional[int]=None, fields: dict=None,
+                clear_on_submit: bool=False) -> tuple:
+            """
+            Creates a login widget.
+
+            Parameters
+            ----------
+            location: str
+                Location of the login widget i.e. main or sidebar.
+            max_concurrent_users: int
+                Maximum number of users allowed to login concurrently.
+            max_login_attempts: int
+                Maximum number of failed login attempts a user can make.
+            fields: dict
+                Rendered names of the fields/buttons.
+            clear_on_submit: bool
+                Clear on submit setting, True: clears inputs on submit, False: keeps inputs on submit.
+
+            Returns
+            -------
+            str
+                Name of the authenticated user.
+            bool
+                Status of authentication, None: no credentials entered, 
+                False: incorrect credentials, True: correct credentials.
+            str
+                Username of the authenticated user.
+            """
+            if fields is None:
+                fields = {'Form name':'Custom Login', 'Email':'Email', 'Password':'Password','Login':'Login'}
+            if location not in ['main', 'sidebar']:
+                # Temporary deprecation error to be displayed until a future release
+                raise DeprecationError("""Likely deprecation error, the 'form_name' parameter has been replaced with the 'fields' parameter. For further information please 
+                    refer to https://github.com/mkhorasani/Streamlit-Authenticator/tree/main?tab=readme-ov-file#authenticatelogin""")
+                # raise ValueError("Location must be one of 'main' or 'sidebar'")
+            if not st.session_state['authentication_status']:
+                token = self.cookie_handler.get_cookie()
+                if token:
+                    self.authentication_handler.execute_login(token=token)
+                time.sleep(0.7)
+                if not st.session_state['authentication_status']:
+                    login_form = st.form('Login', clear_on_submit=clear_on_submit)
+                    
+                    login_form.subheader('Login' if 'Form name' not in fields else fields['Form name'])
+                    email = login_form.text_input('Email' if 'Email' not in fields
+                        else fields['Email']).lower()
+                    password = login_form.text_input('Password' if 'Password' not in fields
+                        else fields['Password'], type='password')
+                    if login_form.form_submit_button('Login' if 'Login' not in fields
+                        else fields['Login']):
+                        if self.authentication_handler.check_credentials(email, password):
+                            self.authentication_handler.execute_login(email=email)
+                            self.cookie_handler.set_cookie()
+            return (st.session_state['name'], st.session_state['authentication_status'],
+                    st.session_state['email'])
+
+# ---------- custom login widget end ------------
 
 # ------ USER AUTHENTICATION ----- #
 # Import the YAML dummy file
@@ -33,16 +114,20 @@ with session_file_path.open("rb") as file:
     sessions = yaml.load(file, Loader=SafeLoader)
 
 # Create the authenticator object
-authenticator = stauth.Authenticate(    
-    config['credentials'],
-    config['cookie']['name'],
-    config['cookie']['key'],
-    config['cookie']['expiry_days'],
-    config['preauthorized']
-)
+# authenticator = stauth.Authenticate(    
+#     config['credentials'],
+#     config['cookie']['name'],
+#     config['cookie']['key'],
+#     config['cookie']['expiry_days'],
+#     config['preauthorized']
+# )
 
-# retrieve the name, authentication status, and username from Streamlit's session state using st.session_state["name"], st.session_state["authentication_status"], and st.session_state["username"]
-authenticator.login()
+# # retrieve the name, authentication status, and username from Streamlit's session state using st.session_state["name"], st.session_state["authentication_status"], and st.session_state["username"]
+# authenticator.login()
+
+# custom
+cauth = CustomAuthenticate()
+cauth.customLogin()
 
 if st.session_state["authentication_status"] == False:
     st.error("Username/password is incorrect")
@@ -52,7 +137,6 @@ if st.session_state["authentication_status"] == None:
 
 if st.session_state["authentication_status"]: # USER AUTHENTICATION is success
 
-    
 
 # ------ MAIN PAGE ----- #
 
@@ -69,7 +153,7 @@ if st.session_state["authentication_status"]: # USER AUTHENTICATION is success
 
     with st.sidebar:
         # buttons
-        authenticator.logout()  
+        cauth.logout()  
 
         # generate session buttons
         st.title("Chatrooms")
---
+++ customAuthentication/__init__.py
@@ -0,0 +1,444 @@
+"""
+Script description: This module executes the logic for the login, logout, register user,
+reset password, forgot password, forgot username, and modify user details widgets. 
+
+Libraries imported:
+- streamlit: Framework used to build pure Python web applications.
+- typing: Module implementing standard typing notations for Python functions.
+"""
+
+from typing import Optional
+import streamlit as st
+
+from streamlit_authenticator.utilities.hasher import Hasher
+from streamlit_authenticator.utilities.validator import Validator
+from streamlit_authenticator.utilities.helpers import Helpers
+from streamlit_authenticator.utilities.exceptions import (CredentialsError,
+                                  ForgotError,
+                                  LoginError,
+                                  RegisterError,
+                                  ResetError,
+                                  UpdateError)
+
+
+import requests
+from apis import apis
+
+class CustomAuthenticationHandler:
+    """
+    This class will execute the logic for the login, logout, register user, reset password, 
+    forgot password, forgot username, and modify user details widgets.
+    """
+    login_t = None
+
+    def __init__(self, validator: Optional[Validator]=None):
+        """
+        Create a new instance of "AuthenticationHandler".
+
+        Parameters
+        ----------
+        credentials: dict
+            Dictionary of usernames, names, passwords, emails, and other user data.
+        pre-authorized: list
+            List of emails of unregistered users who are authorized to register.        
+        validator: Validator
+            Validator object that checks the validity of the username, name, and email fields.
+        """
+        # self.credentials                =   credentials
+        # self.pre_authorized             =   pre_authorized
+        # self.credentials['usernames']   =   {
+        #                                     key.lower(): value
+        #                                     for key, value in credentials['usernames'].items()
+        #                                     }
+        self.validator                  =   validator if validator is not None else Validator()
+        # self.random_password            =   None
+
+        # for username, _ in self.credentials['usernames'].items():
+        #     if 'logged_in' not in self.credentials['usernames'][username]:
+        #         self.credentials['usernames'][username]['logged_in'] = False
+        #     if 'failed_login_attempts' not in self.credentials['usernames'][username]:
+        #         self.credentials['usernames'][username]['failed_login_attempts'] = 0
+        #     if not Hasher._is_hash(self.credentials['usernames'][username]['password']):
+        #         self.credentials['usernames'][username]['password'] = \
+        #             Hasher._hash(self.credentials['usernames'][username]['password'])
+        if 'name' not in st.session_state:
+            st.session_state['name'] = None
+        if 'authentication_status' not in st.session_state:
+            st.session_state['authentication_status'] = None
+        if 'email' not in st.session_state:
+            st.session_state['email'] = None
+        if 'logout' not in st.session_state:
+            st.session_state['logout'] = None
+    def check_credentials(self, email: str, password: str) -> bool:
+        """
+        Checks the validity of the entered credentials.
+
+        Parameters
+        ----------
+        email: str
+            The entered email.
+        password: str
+            The entered password.
+
+        Returns
+        -------
+        bool
+            Validity of the entered credentials.
+        """
+        if email is not None:
+            try:
+                # --------- sending login requests ---------
+                test = {"email": email, "password": password}
+                login_r = requests.post(apis.get("LOGIN"), data=test)
+                "login_r:", login_r      # for testing
+                if hasattr(login_r, 'error') == None:
+                    self.login_t = login_r.token
+                    # st.session_state['authentication_status'] = True
+                    return True
+                else:
+                    # st.session_state['authentication_status'] = False
+                    return False
+            except TypeError as e:
+                print(e)
+            except ValueError as e:
+                print(e)
+        else:
+            st.session_state['authentication_status'] = False
+            return False
+        return None
+    def _count_concurrent_users(self) -> int:
+        """
+        Counts the number of users logged in concurrently.
+
+        Returns
+        -------
+        int
+            Number of users logged in concurrently.
+        """
+        concurrent_users = 0
+        for username, _ in self.credentials['usernames'].items():
+            if self.credentials['usernames'][username]['logged_in']:
+                concurrent_users += 1
+        return concurrent_users
+    def _credentials_contains_value(self, value: str) -> bool:
+        """
+        Checks to see if a value is present in the credentials dictionary.
+
+        Parameters
+        ----------
+        value: str
+            Value being checked.
+
+        Returns
+        -------
+        bool
+            Presence/absence of the value, True: value present, False value absent.
+        """
+        return any(value in d.values() for d in self.credentials['usernames'].values())
+    
+    def execute_login(self, email: Optional[str]=None, token: Optional[dict]=None):
+        """
+        Executes login by setting authentication status to true and adding the user's
+        username and name to the session state.
+
+        Parameters
+        ----------
+        email: str
+            The email of the user being logged in.
+        token: dict
+            The re-authentication cookie to retrieve the username from.
+        """
+        if st.session_state['FormSubmitter:Login-Login'] == True:
+            st.session_state['email'] = email
+            st.session_state['name'] =  self.login_t # self.credentials['email'][email]['name'] 
+            st.session_state['authentication_status'] = True
+            # self._record_failed_login_attempts(username, reset=True)
+            # self.credentials['emails'][email]['logged_in'] = True
+        # elif token:
+        #     st.session_state['username'] = token['username']
+        #     st.session_state['name'] = self.credentials['usernames'][token['username']]['name']
+        #     st.session_state['authentication_status'] = True
+        #     self.credentials['usernames'][token['username']]['logged_in'] = True
+
+    def execute_logout(self):
+        """
+        Clears cookie and session state variables associated with the logged in user.
+        """
+        self.credentials['usernames'][st.session_state['username']]['logged_in'] = False
+        st.session_state['logout'] = True
+        st.session_state['name'] = None
+        st.session_state['username'] = None
+        st.session_state['authentication_status'] = None
+    def forgot_password(self, username: str) -> tuple:
+        """
+        Creates a new random password for the user.
+
+        Parameters
+        ----------
+        username: str
+            Username associated with the forgotten password.
+
+        Returns
+        -------
+        tuple
+            Username of the user; email of the user; new random password of the user.
+        """
+        if not self.validator.validate_length(username, 1):
+            raise ForgotError('Username not provided')
+        if username in self.credentials['usernames']:
+            return (username, self.credentials['usernames'][username]['email'],
+                    self._set_random_password(username))
+        else:
+            return False, None, None
+    def forgot_username(self, email: str) -> tuple:
+        """
+        Retrieves the forgotten username of a user.
+
+        Parameters
+        ----------
+        email: str
+            Email associated with the forgotten username.
+
+        Returns
+        -------
+        tuple
+            Username of the user; email of the user.
+        """
+        if not self.validator.validate_length(email, 1):
+            raise ForgotError('Email not provided')
+        return self._get_username('email', email), email
+    def _get_username(self, key: str, value: str) -> str:
+        """
+        Retrieves the username based on a provided entry.
+
+        Parameters
+        ----------
+        key: str
+            Name of the credential to query i.e. "email".
+        value: str
+            Value of the queried credential i.e. "jsmith@gmail.com".
+
+        Returns
+        -------
+        str
+            Username associated with the given key, value pair i.e. "jsmith".
+        """
+        for email, values in self.credentials['usernames'].items():
+            if values[key] == value:
+                return email
+        return False
+    def _record_failed_login_attempts(self, email: str, reset: bool=False):
+        """
+        Records the number of failed login attempts for a given username.
+        
+        Parameters
+        ----------
+        reset: bool            
+            Reset failed login attempts option, True: number of failed login attempts
+            for the user will be reset to 0, 
+            False: number of failed login attempts for the user will be incremented.
+        """
+        if reset:
+            self.credentials['emails'][email]['failed_login_attempts'] = 0
+        else:
+            self.credentials['emails'][email]['failed_login_attempts'] += 1
+    def _register_credentials(self, email: str, name: str, password: str, pre_authorization: bool, domains: list):
+        """
+        Adds the new user's information to the credentials dictionary.
+
+        Parameters
+        ----------
+        username: str
+            Username of the new user.
+        name: str
+            Name of the new user.
+        password: str
+            Password of the new user.
+        email: str
+            Email of the new user.
+        pre-authorization: bool
+            Pre-authorization requirement, True: user must be pre-authorized to register, 
+            False: any user can register.
+        domains: list
+            Required list of domains a new email must belong to i.e. ['gmail.com', 'yahoo.com'], 
+            list: the required list of domains, None: any domain is allowed.
+        """
+        if not self.validator.validate_email(email):
+            raise RegisterError('Email is not valid')
+        if self._credentials_contains_value(email):
+            raise RegisterError('Email already taken')
+        if domains:
+            if email.split('@')[1] not in ' '.join(domains):
+                raise RegisterError('Email not allowed to register')
+        if not self.validator.validate_username(username):
+            raise RegisterError('Username is not valid')
+        if username in self.credentials['usernames']:
+            raise RegisterError('Username already taken')
+        if not self.validator.validate_name(name):
+            raise RegisterError('Name is not valid')
+        self.credentials['usernames'][username] = \
+            {'name': name, 'password': Hasher([password]).generate()[0], 'email': email,
+             'logged_in': False}
+        if pre_authorization:
+            self.pre_authorized['emails'].remove(email)
+    def register_user(self, new_password: str, new_password_repeat: str, pre_authorization: bool,
+                      new_username: str, new_name: str, new_email: str,
+                      domains: Optional[list]=None) -> tuple:
+        """
+        Validates a new user's username, password, and email. Subsequently adds the validated user 
+        details to the credentials dictionary.
+
+        Parameters
+        ----------
+        new_password: str
+            Password of the new user.
+        new_password_repeat: str
+            Repeated password of the new user.
+        pre-authorization: bool
+            Pre-authorization requirement, True: user must be pre-authorized to register, 
+            False: any user can register.
+        new_username: str
+            Username of the new user.
+        new_name: str
+            Name of the new user.
+        new_email: str
+            Email of the new user.
+        domains: list
+            Required list of domains a new email must belong to i.e. ['gmail.com', 'yahoo.com'], 
+            list: the required list of domains, None: any domain is allowed.
+
+        Returns
+        -------
+        tuple
+            Email of the new user; username of the new user; name of the new user.
+        """
+        if not self.validator.validate_length(new_password, 1) \
+            or not self.validator.validate_length(new_password_repeat, 1):
+            raise RegisterError('Password/repeat password fields cannot be empty')
+        if new_password != new_password_repeat:
+            raise RegisterError('Passwords do not match')
+        if pre_authorization:
+            if new_email in self.pre_authorized['emails']:
+                self._register_credentials(new_username, new_name, new_password, new_email,
+                                            pre_authorization, domains)
+                return new_email, new_username, new_name
+            else:
+                raise RegisterError('User not pre-authorized to register')
+        else:
+            self._register_credentials(new_username, new_name, new_password, new_email,
+                                        pre_authorization, domains)
+            return new_email, new_username, new_name
+
+    def reset_password(self, username: str, password: str, new_password: str,
+                       new_password_repeat: str) -> bool:
+        """
+        Validates the user's current password and subsequently saves their new password to the 
+        credentials dictionary.
+
+        Parameters
+        ----------
+        username: str
+            Username of the user.
+        password: str
+            Current password of the user.
+        new_password: str
+            New password of the user.
+        new_password_repeat: str
+            Repeated new password of the user.
+
+        Returns
+        -------
+        bool
+            State of resetting the password, True: password reset successfully.
+        """
+        if self.check_credentials(username, password):
+            if not self.validator.validate_length(new_password, 1):
+                raise ResetError('No new password provided')
+            if new_password != new_password_repeat:
+                raise ResetError('Passwords do not match')
+            if password != new_password:
+                self._update_password(username, new_password)
+                return True
+            else:
+                raise ResetError('New and current passwords are the same')
+        else:
+            raise CredentialsError('password')
+    def _set_random_password(self, username: str) -> str:
+        """
+        Updates the credentials dictionary with the user's hashed random password.
+
+        Parameters
+        ----------
+        username: str
+            Username of the user to set the random password for.
+
+        Returns
+        -------
+        str
+            New plain text password that should be transferred to the user securely.
+        """
+        self.random_password = Helpers.generate_random_pw()
+        self.credentials['usernames'][username]['password'] = \
+            Hasher([self.random_password]).generate()[0]
+        return self.random_password
+    def _update_entry(self, username: str, key: str, value: str):
+        """
+        Updates the credentials dictionary with the user's updated entry.
+
+        Parameters
+        ----------
+        username: str
+            Username of the user to update the entry for.
+        key: str
+            Updated entry key i.e. "email".
+        value: str
+            Updated entry value i.e. "jsmith@gmail.com".
+        """
+        self.credentials['usernames'][username][key] = value
+    def _update_password(self, username: str, password: str):
+        """
+        Updates the credentials dictionary with the user's hashed reset password.
+
+        Parameters
+        ----------
+        username: str
+            Username of the user to update the password for.
+        password: str
+            Updated plain text password.
+        """
+        self.credentials['usernames'][username]['password'] = Hasher([password]).generate()[0]
+    def update_user_details(self, new_value: str, username: str, field: str) -> bool:
+        """
+        Validates the user's updated name or email and subsequently modifies it in the
+        credentials dictionary.
+
+        Parameters
+        ----------
+        new_value: str
+            New value for the name or email.
+        username: str
+            Username of the user.
+        field: str
+            Field to update i.e. name or email.
+
+        Returns
+        -------
+        bool
+            State of updating the user's detail, True: details updated successfully.
+        """
+        if field == 'name':
+            if not self.validator.validate_name(new_value):
+                raise UpdateError('Name is not valid')
+        if field == 'email':
+            if not self.validator.validate_email(new_value):
+                raise UpdateError('Email is not valid')
+            if self._credentials_contains_value(new_value):
+                raise UpdateError('Email already taken')
+        if new_value != self.credentials['usernames'][username][field]:
+            self._update_entry(username, field, new_value)
+            if field == 'name':
+                st.session_state['name'] = new_value
+            return True
+        else:
+            raise UpdateError('New and current values are the same')
+        
