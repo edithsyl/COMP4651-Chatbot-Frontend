5907ba720a0db4551417fff9b5ff38ff0f0145bc
syleung <syleung1220@gmail.com>
2024-05-14 11:44:09 +0800

    update endpoints

--- apis.py
+++ apis.py
@@ -1,8 +1,12 @@
 apis = {
-    "CHAT": "https://httpbin.org/get",# "https://v1/chat",
-    "TRANSLATE": "https://httpbin.org/get",# "https://v1/translate",
+    # "CHAT": "https://httpbin.org/get",# "https://v1/chat",
+    # "TRANSLATE": "https://httpbin.org/get",# "https://v1/translate",
     # "DRAW":  "https://httpbin.org/get",# "https://v1/media",
     # "MUSIC": "https://httpbin.org/get",#  "https://v1/music",
+    "SIGNUP": "http://127.0.0.1:8080/function/sign-up",
     "LOGIN": "http://127.0.0.1:8080/function/login",
+    "CREATE-CHAT-SESSIONS": "http://127.0.0.1:8080/function/create-chat-session",
     "GET-CHAT-SESSIONS": "http://127.0.0.1:8080/function/get-chat-sessions",
+    "GET-CHAT-HISTORIES": "http://127.0.0.1:8080/function/get-chat-histories",
+    "GET-CHAT": "http://localhost:8080/function/chat",
 }
